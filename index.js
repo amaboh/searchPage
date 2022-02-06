31a935bcedb00f5100c626cccb2a9e6ef07796a7
amaboh <amabocanon@gmail.com>
2022-02-06 22:27:36 +0100

    changes index.js and index.html

---
+++ index.js
@@ -0,0 +1,14 @@
+document.getElementById("search-input").addEventListener("keyup", function(event){
+ let userInput = event.target.value.toLowerCase();
+ let dataArray = document.getElementsByClassName("name");
+
+ for(let i=0; i < dataArray.length; i++){
+     const presidentName = dataArray[i].textContent.toLowerCase;
+
+     if(presidentName.includes(userInput)){
+         dataArray[i].style.display = "block"
+     } else{
+         dataArray[i].style.display = "none"
+     }
+ }
+})
